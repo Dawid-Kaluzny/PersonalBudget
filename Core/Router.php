a1d6b186dbf13c005bafdf7a8cca24280f065484
Dawid Kaluzny <dawid.kaluzny.programista@gmail.com>
2021-03-03 20:45:51 +0100

    Controllers and actions

--- Core/Router.php
+++ Core/Router.php
@@ -1,5 +1,7 @@
 <?php
 
+namespace Core;
+
 /**
  * Router
  *
@@ -88,5 +90,119 @@ class Router
 	public function getParams()
 	{
 		return $this->params;
+	}
+	
+	/**
+	 * Dispatch teh route, creating the controller object and running the action method
+	 *
+	 * @param string $url The route URL
+	 *
+	 * @return void
+	 */
+	public function dispatch($url)
+	{
+		$url = $this->removeQueryStringVariables($url);
+		
+		if ($this->match($url)) {
+			$controller = $this->params['controller'];
+			$controller = $this->convertToStudlyCaps($controller);
+			$controller = $this->getNamespace() . $controller;
+			
+			if (class_exists($controller)) {
+				$controller_object = new $controller($this->params);
+				
+				$action = $this->params['action'];
+				$action = $this->convertToCamelCase($action);
+				
+				if (preg_match('/action$/i', $action) == 0) {
+					$controller_object->$action();
+					
+				} else {
+                    throw new \Exception("Method $action in controller $controller cannot be called directly - remove the Action suffix to call this method");
+				}
+			} else {
+				echo "Controller class $controller not found";
+			}	
+		} else {
+			echo "No route matched";
+		}
+	}
+	
+	/**
+	 * Convert the string with hyphens to StudlyCaps, e.g. post-authors => PostAuthors
+	 *
+	 * @param string $string The string to convert
+	 *
+	 * @return string
+	 */
+	protected function convertToStudlyCaps($string)
+	{
+		return str_replace(' ', '', ucwords(str_replace('-', ' ', $string)));
+	}
+	
+	/**
+	 * Convert the string with hyphens to camelCase, e.g. add-new => addNew
+	 *
+	 * @param string $string The string to convert
+	 *
+	 * @return string
+	 */
+	protected function convertToCamelCase($string)
+	{
+		return lcfirst($this->convertToStudlyCaps($string));
+	}
+	
+	/**
+     * Remove the query string variables from the URL (if any). As the full
+     * query string is used for the route, any variables at the end will need
+     * to be removed before the route is matched to the routing table. For
+     * example:
+     *
+     *   URL                           $_SERVER['QUERY_STRING']  Route
+     *   -------------------------------------------------------------------
+     *   localhost                     ''                        ''
+     *   localhost/?                   ''                        ''
+     *   localhost/?page=1             page=1                    ''
+     *   localhost/posts?page=1        posts&page=1              posts
+     *   localhost/posts/index         posts/index               posts/index
+     *   localhost/posts/index?page=1  posts/index&page=1        posts/index
+     *
+     * A URL of the format localhost/?page (one variable name, no value) won't
+     * work however. (NB. The .htaccess file converts the first ? to a & when
+     * it's passed through to the $_SERVER variable).
+     *
+     * @param string $url The full URL
+     *
+     * @return string The URL with the query string variables removed
+     */
+	protected function removeQueryStringVariables($url)
+	{
+		if ($url != '') {
+			$parts = explode('&', $url, 2);
+			
+			if (strpos($parts[0], '=') === false) {
+				$url = $parts[0];
+			} else {
+				$url = '';
+			}
+		}
+		
+		return $url;
+	}
+	
+	/**
+	 * Get the namespace for the controller class. The namespace defined in the route parameters is added if present
+	 *
+	 * @return string The request URL
+	 */
+	protected function getNamespace()
+	{
+		$namespace = 'App\Controllers\\';
+		
+		if (array_key_exists('namespace', $this->params)) {
+			$namespace .= $this->params['namespace'] . '\\';
+		}
+		
+		return $namespace;
 	}
 }
--- public/index.php
+++ public/index.php
@@ -5,25 +5,25 @@
  *
  * PHP version 7.0
  */
- 
+
+/**
+ * Autoloader
+ */
+spl_autoload_register(function($class) {
+	$root = dirname(__DIR__); // get the parent directory
+	$file = $root . '/' . str_replace('\\', '/', $class) . '.php';
+	if (is_readable($file)) {
+		require $root . '/' . str_replace('\\', '/', $class) . '.php';
+	}
+});
+
 /**
  * Routing
  */
-require '../Core/Router.php';
-
-$router = new Router();
+$router = new Core\Router();
 
 // Add the routes
 $router->add('', ['controller' => 'Home', 'action' => 'index']);
 $router->add('{controller}/{action}');
 
-// Match the requested route
-$url = $_SERVER['QUERY_STRING'];
-
-if ($router->match($url)) {
-	echo '<pre>';
-	var_dump($router->getParams());
-	echo '</pre>';
-} else {
-	echo "No route found for URL '$url'";
-}
+$router->dispatch($_SERVER['QUERY_STRING']);
