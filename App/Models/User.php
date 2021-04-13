24ca988d2a9ff249e95ac513571dd48946b2686d
Dawid Kaluzny <dawid.kaluzny.programista@gmail.com>
2021-04-13 20:28:05 +0200

    Account activation

--- App/Models/User.php
+++ App/Models/User.php
@@ -48,7 +48,11 @@ class User extends \Core\Model
 			
 			$password_hash = password_hash($this->password, PASSWORD_DEFAULT);
 			
-			$sql = 'INSERT INTO users (name, email, password) VALUES (:name, :email, :password)';
+			$token = new Token();
+			$hashed_token = $token->getHash();
+			$this->activation_token = $token->getValue();
+			
+			$sql = 'INSERT INTO users (name, email, password, activation_hash) VALUES (:name, :email, :password, :activation_hash)';
 			
 			$db = static::getDB();
 			$stmt = $db->prepare($sql);
@@ -56,6 +60,7 @@ class User extends \Core\Model
 			$stmt->bindValue(':name', $this->name, PDO::PARAM_STR);
 			$stmt->bindValue(':email', $this->email, PDO::PARAM_STR);
 			$stmt->bindValue(':password', $password_hash, PDO::PARAM_STR);
+			$stmt->bindValue(':activation_hash', $hashed_token, PDO::PARAM_STR);
 			
 			return $stmt->execute();
 		}
@@ -157,7 +162,7 @@ class User extends \Core\Model
 	{
 		$user = static::findByEmail($email);
 		
-		if ($user) {
+		if ($user && $user->is_active) {
 			if (password_verify($password, $user->password)) {
 				return $user;
 			}
@@ -340,5 +345,42 @@ class User extends \Core\Model
 		}
 		
 		return false;
+	}
+	
+	/**
+	 * Send an email to the user containing the activation link
+	 *
+	 * @return void
+	 */
+	public function sendActivationEmail()
+	{
+		$url = 'http://' . $_SERVER['HTTP_HOST'] . '/signup/activate/' . $this->activation_token;
+		
+		$text = View::getTemplate('Signup/activation_email.txt', ['url' => $url]);
+		$html = View::getTemplate('Signup/activation_email.html', ['url' => $url]);
+		
+		Mail::send($this->email, 'Aktywacja konta', $text, $html);
+	}
+			
+	/**
+	 * Activate the user account with the specified activation token
+	 *
+	 * @param string $value Activation token from the URL
+	 *
+	 * @return void
+	 */
+	public static function activate($value)
+	{
+		$token = new Token($value);
+		$hashed_token = $token->getHash();
+		
+		$sql = 'UPDATE users SET is_active = 1, activation_hash = null WHERE activation_hash = :hashed_token';
+		
+		$db = static::getDB();
+		$stmt = $db->prepare($sql);
+			
+		$stmt->bindValue(':hashed_token', $hashed_token, PDO::PARAM_STR);
+		
+		$stmt->execute();
 	}
 }
